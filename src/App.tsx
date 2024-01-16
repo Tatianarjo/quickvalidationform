c21f9997c1cca7bed2432761695398044babba14
Tatiana Johnson <tatianarjo@gmail.com>
2024-01-16 16:10:06 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormInput from './components/FormInput';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="app">
+    <form>
+      <FormInput placeholder="Enter city, point of interest, or address"/>
+      <FormInput placeholder="Select your dates"/>
+      <FormInput placeholder="Rooms"/>
+      <FormInput placeholder="Attendees" />
+    </form>
+  </div>
 }
 
 export default App;
---
+++ src/components/FormInput.tsx
@@ -0,0 +1,12 @@
+import "./formInput.css"
+
+const FormInput = (props: { placeholder: string | undefined }) => {
+    return (
+        <div className="formInput">
+            <label>Username</label>
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default FormInput
